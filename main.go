52396ddc5c87d9cf28321db7fbdeeff05f2b75c8
Guilherme R Woelke <guilherme_wolke@yahoo.com>
2023-02-09 17:04:14 -0300

    main
    - Alteração da abordagem, usando channels para verificar receber o
      primeiro retorno, e encerrar o processo assim que a mensagem chegar

--- main.go
+++ main.go
@@ -17,20 +17,28 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*1)
 	defer cancel()
 
+	apicepChannel := make(chan types.ApiCEP)
+	viacepChannel := make(chan types.ViaCEP)
+
 	cep := "08773-160"
 	wg := sync.WaitGroup{}
-	wg.Add(1)
-	go ApiCEP(ctx, cep, &wg)
-	go ViaCEP(ctx, cep, &wg)
+	wg.Add(2)
+
+	go ApiCEP(ctx, cep, &wg, apicepChannel)
+	go ViaCEP(ctx, cep, &wg, viacepChannel)
 
 	select {
+	case msg := <-apicepChannel:
+		log.Printf("ApiCEP: %#v", msg)
+	case msg := <-viacepChannel:
+		log.Printf("ViaCEP: %#v", msg)
 	case <-ctx.Done():
 		log.Println("Contexto cancelado")
 	}
 
 }
 
-func ApiCEP(ctx context.Context, cep string, wg *sync.WaitGroup) {
+func ApiCEP(ctx context.Context, cep string, wg *sync.WaitGroup, ch chan<- types.ApiCEP) {
 	endpoint := fmt.Sprintf("https://cdn.apicep.com/file/apicep/%s.json", cep)
 
 	body, err := DoRequest(ctx, endpoint)
@@ -45,10 +53,12 @@ func ApiCEP(ctx context.Context, cep string, wg *sync.WaitGroup) {
 	}
 
 	fmt.Printf("APICEP - resultado da consulta: %#v", apicep)
+	ch <- apicep
 	wg.Done()
+	close(ch)
 }
 
-func ViaCEP(ctx context.Context, cep string, wg *sync.WaitGroup) {
+func ViaCEP(ctx context.Context, cep string, wg *sync.WaitGroup, ch chan<- types.ViaCEP) {
 	endpoint := fmt.Sprintf("https://viacep.com.br/ws/%s/json", cep)
 
 	body, err := DoRequest(ctx, endpoint)
@@ -61,8 +71,10 @@ func ViaCEP(ctx context.Context, cep string, wg *sync.WaitGroup) {
 	if err = json.Unmarshal(body, &viacep); err != nil {
 		panic(err)
 	}
+	ch <- viacep
 	fmt.Printf("ViaCEP - resultado da consulta: %#v", viacep)
 	wg.Done()
+	close(ch)
 }
 
 func DoRequest(ctx context.Context, endpoint string) ([]byte, error) {
